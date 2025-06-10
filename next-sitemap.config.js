79c84cd6103d544a96f3856597659a2149c2dca4
Mathis Kräkel <mathis.kraekel@gmail.com>
2025-06-10 19:19:53 +0200

    Updaed sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://mathiskraekel.de",
+    generateRobotsTxt: true,
+    changefreq: "weekly",
+    priority: 0.7,
+    sitemapSize: 5000,
+};
